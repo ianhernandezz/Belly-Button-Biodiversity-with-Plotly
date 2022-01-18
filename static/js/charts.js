7ddd7cc9f0746e28f34cfa4cad7adb253d0ba271
Ian Hernandez <ian.hernandezz@outlook.com>
2022-01-18 13:06:04 -0500

    commit

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,8 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("./static/data/samples.json").then((data) => {
+    console.log(data);
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +33,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("./static/data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,7 +57,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("./static/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
@@ -71,7 +72,7 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    //var yticks = 
 
     // 8. Create the trace for the bar chart. 
     var barData = [
