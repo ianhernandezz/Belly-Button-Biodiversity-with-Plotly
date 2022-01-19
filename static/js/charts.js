73415b5c9fc506c59129a6f1b0c9d8a8afdd3806
Ian Hernandez <ian.hernandezz@outlook.com>
2022-01-19 14:19:29 -0500

    commit

--- static/js/charts.js
+++ static/js/charts.js
@@ -104,6 +104,39 @@ function buildCharts(sample) {
      title: `Top Ten Bacteria Cultures Found in Sample ${sample}`
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout)
+    Plotly.newPlot("bar", barData, barLayout);
+
+
+    // Bar and Bubble charts
+    // Create the buildCharts function.
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      {
+        x: otu_ids, 
+        y: sample_values, 
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+        }
+      }
+   
+        ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: `All Bacteria Cultures Per Sample ${sample}`,
+      showlegend: false, 
+      xaxis: {title: "OTU ID"},
+      yaxis: {title: "Bacteria Count"},
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
   });
+  // samples.json no longer available 
 }
